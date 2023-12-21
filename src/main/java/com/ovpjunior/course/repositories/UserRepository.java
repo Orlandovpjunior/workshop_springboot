b1ffeafe9d745b3f6a96ed6a528568e38794f218
Orlandovpjunior <orlando.virginio.penha.junior@ccc.ufcg.edu.br>
2023-12-21 20:04:46 -0300

    Service layer

--- src/main/java/com/ovpjunior/course/repositories/UserRepository.java
+++ src/main/java/com/ovpjunior/course/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.ovpjunior.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ovpjunior.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
